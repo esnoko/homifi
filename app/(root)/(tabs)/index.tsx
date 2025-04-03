a7e38997f938f237f1049d26f32086102956ee58
esnoko <elvisnoko18@gmail.com>
2025-04-03 21:50:00 +0200

    Refactor UI layout and configuration files

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,12 +11,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>
+      <Text className="font-bold text-lg my-10">Welcome to Homifi</Text>
         <Link href="/signin">Sign-In</Link>
         <Link href="/explore">Explore</Link>
         <Link href="/profile">Profile</Link>
         <Link href="/properties/1">Property</Link>
-      </Text>
     </View>
   );
 }
