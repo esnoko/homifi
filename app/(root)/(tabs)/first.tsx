4e691df396f769216981243078372a8bf51f3ac7
esnoko <elvisnoko18@gmail.com>
2025-04-05 02:24:12 +0200

    Refactor UI layout by removing unused files and updating welcome message font size

--- app/(root)/(tabs)/first.tsx
+++ app/(root)/(tabs)/first.tsx
@@ -12,7 +12,6 @@ export default function Index() {
       }}
     >
       <Text className="font-bold text-3xl my-10 font-rubik">Welcome to Homifi</Text>
-        <Link href="/index">Index</Link>
         <Link href="/explore">Explore</Link>
         <Link href="/profile">Profile</Link>
         <Link href="/properties/1">Property</Link>
