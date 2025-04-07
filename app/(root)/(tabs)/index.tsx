0690892135ce18588facb56d9e5110369b7d9e51
esnoko <elvisnoko18@gmail.com>
2025-04-07 13:11:11 +0200

    Refactor tab navigation in the UI layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -12,10 +12,6 @@ export default function Index() {
       }}
     >
       <Text className="font-bold text-3xl my-10 font-rubik">Welcome to Homifi</Text>
-        <Link href="/signin">Signin</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
     </View>
   );
 }
