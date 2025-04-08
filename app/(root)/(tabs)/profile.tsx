4a852a007a0222a71a3e4e6133cc0312c3aafec2
esnoko <elvisnoko18@gmail.com>
2025-04-08 14:25:03 +0200

    Refactor profile UI layout and add profile image editing functionality

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,37 +1,56 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import icons from '@/constants/icons'
-import images from '@/constants/images'
+import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+
+interface SettingsItemProps{
+  icon: ImageSourcePropType;
+  title: string;
+  onPress: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({icon, title, onPress, textStyle, showArrow= true}: SettingsItemProps) => (
+  <TouchableOpacity>
+    <View>
+      <Image source={icon}/>
+      <Text>{title}</Text>
+    </View>
+  </TouchableOpacity>
+)
 
 const profile = () => {
   return (
-    <SafeAreaView className='h-full bg-white'>
+    <SafeAreaView className="h-full bg-white">
       <ScrollView
-         showsVerticalScrollIndicator={false}
-         contentContainerClassName='pb-32 px-7'
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
       >
-
-
-        <View className='flex flex-row items-center justify-between mt-5'>
-          <Text className='text-xl font-rubik-bold'>Profile</Text>
-          <Image source={icons.bell} className='size-5'/>
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
         </View>
 
-        <View className='flex-row justify-center flex mt-5'>
-          <View className='flex flex-col items-center relative mt-5'>
-            <Image source={images.avatar} className='size-44 relative rounded-full'/>
-            <TouchableOpacity className='absolute bottom-11 right-2'>
-              <Image source={icons.edit} className='size-9'/>
-            </TouchableOpacity> 
-            <Text className='text-2xl font-rubik-bold mt-2'>
+        <View className="flex-row justify-center flex mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <Image
+              source={images.avatar}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
+            <Text className="text-2xl font-rubik-bold mt-2">
               NokoDev | ELNO
             </Text>
           </View>
+          <View className="flex flex-col mt-10"></View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default profile
+export default profile;
