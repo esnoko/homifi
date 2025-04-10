e8b20341376d4dbe534f0024ddf7b4f8a1248e61
esnoko <elvisnoko18@gmail.com>
2025-04-10 20:48:41 +0200

    Refactor profile UI layout, add profile image editing functionality, and update user name display

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
 import React from "react";
@@ -6,9 +7,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-  <SafeAreaView>
-    <View className="bg-white h-full">
-      <View className="px-5">
+  <SafeAreaView className="bg-white h-full" >
+    <View className="px-5">
+      <View className="flex flex-row items-center justify-between mt-5" >
         <View className="flex flex-row items-center">
           <Image source={images.avatar} className="size-12 rounded-full"/>
           <View className="flex flex-col items-start ml-2 justify-center">
@@ -16,6 +17,7 @@ export default function Index() {
             <Text className="text-base font-rubik-medium text-black-300">NokoDev</Text>
           </View>
         </View>
+        <Image source={icons.bell} className="size-6"/>
       </View>
     </View>
   </SafeAreaView>
