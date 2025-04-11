6dd09b7e3662852a90248959cb573d277267d890
esnoko <elvisnoko18@gmail.com>
2025-04-12 00:06:16 +0200

    Refactor Cards component to update FeaturedCards styling and content

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -35,16 +35,18 @@ export const Cards = ({onPress}: Props) => {
       <TouchableOpacity onPress={onPress} className='flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative'>
          <View className='flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50'>
             <Image source={icons.star} className='size-2.5'/>
-            <Text className='text-xs font-rubik-bold text-primary-300 ml-1'>4.4</Text>
+            <Text className='text-xs font-rubik-bold text-primary-300 ml-0.5'>4.4</Text>
         </View>
 
-        <View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
-            <Text className='text-xl font-rubik-extrabold text-white' numberOfLines={1}>Modern Apartment</Text>
-            <Text className='text-base font-rubik text-white'>Polokwane LP</Text>
+        <Image source={images.newYork} className='w-full h-40 rounded-lg'/>
 
-            <View className='flex flex-row items-center justify-between w-full'>
-                <Text className='text-white font-rubik-extrabold text-xl'>R2, 000</Text>
-                <Image source={icons.heart} className='size-5'/>
+        <View className='flex flex-col mt-2'>
+            <Text className='text-base font-rubik-bold text-black-300'>Cozy Studio</Text>
+            <Text className='text-xs font-rubik text-black-200'>Polokwane LP</Text>
+
+            <View className='flex flex-row items-center justify-between mt-2'>
+                <Text className='text-base font-rubik-bold text-primary-300'>R2, 000</Text>
+                <Image source={icons.heart} className='w-5 h-5 mr-2' tintColor="#191d31"/>
             </View>
         </View>
       </TouchableOpacity>
