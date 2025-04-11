0368c060d58ec6259793c61c713ee9a5a68d9223
esnoko <elvisnoko18@gmail.com>
2025-04-11 23:58:05 +0200

    Refactor Cards component to update FeaturedCards styling and content

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -30,10 +30,23 @@ export const FeaturedCards = ({onPress}: Props) => {
   )
 }
 
-export const Cards = () => {
+export const Cards = ({onPress}: Props) => {
     return (
-      <View>
-        <Text>Cards</Text>
-      </View>
+      <TouchableOpacity onPress={onPress} className='flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative'>
+         <View className='flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50'>
+            <Image source={icons.star} className='size-2.5'/>
+            <Text className='text-xs font-rubik-bold text-primary-300 ml-1'>4.4</Text>
+        </View>
+
+        <View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
+            <Text className='text-xl font-rubik-extrabold text-white' numberOfLines={1}>Modern Apartment</Text>
+            <Text className='text-base font-rubik text-white'>Polokwane LP</Text>
+
+            <View className='flex flex-row items-center justify-between w-full'>
+                <Text className='text-white font-rubik-extrabold text-xl'>R2, 000</Text>
+                <Image source={icons.heart} className='size-5'/>
+            </View>
+        </View>
+      </TouchableOpacity>
     )
   }
