7013e1cb9bee4d72cdc640fbc799c58c3f993712
esnoko <elvisnoko18@gmail.com>
2025-04-11 23:10:11 +0200

    Refactor Cards component to separate FeaturedCards and Cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Cards = () => {
+export const FeaturedCards = () => {
   return (
     <View>
       <Text>Cards</Text>
@@ -9,4 +9,10 @@ const Cards = () => {
   )
 }
 
-export default Cards
+export const Cards = () => {
+    return (
+      <View>
+        <Text>Cards</Text>
+      </View>
+    )
+  }
