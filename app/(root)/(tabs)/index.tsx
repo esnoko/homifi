54f00889697facce01b135df08612a6fba3f3ea7
esnoko <elvisnoko18@gmail.com>
2025-04-11 23:13:09 +0200

    Refactor styling of FeaturedCards section in Index component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -23,7 +23,7 @@ export default function Index() {
       </View>
     </View>
     <Search/>
-    <View className="my-5">
+    <View className="my-5 m-4">
       <View className="flex flex-row items-center justify-between">
         <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
         <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
