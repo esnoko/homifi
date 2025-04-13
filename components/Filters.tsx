39845da1877a1ee747d6632cb2099d2dcd73cd78
esnoko <elvisnoko18@gmail.com>
2025-04-13 23:57:37 +0200

    Refactor Filters component to add category selection and update layout

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,14 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { categories } from '@/constants/data';
 
 const Filters = () => {
     const params = useLocalSearchParams<{filters?: string}>();
     const [selectedCategory, setSelectedCategory] = useState(params.filters || "All");
+    const handleCategory = (category: string) => {};
   return (
-    <View>
-      <Text>Filters</Text>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt-3 mb-2'>
+        {categories.map((item, index) => (
+            <TouchableOpacity key={index} className='flex flex-col items-start mr-4 px-4 rounded-full'>
+                <Text>{item.title}</Text>
+            </TouchableOpacity>
+        ))}
+    </ScrollView>
   )
 }
 
