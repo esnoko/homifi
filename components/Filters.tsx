6546eb1e637ec8fa958304930f2d434daed446f5
esnoko <elvisnoko18@gmail.com>
2025-04-14 00:04:51 +0200

    Refactor Filters component to update category selection and layout

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,21 +1,35 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
-import { categories } from '@/constants/data';
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { categories } from "@/constants/data";
 
 const Filters = () => {
-    const params = useLocalSearchParams<{filters?: string}>();
-    const [selectedCategory, setSelectedCategory] = useState(params.filters || "All");
-    const handleCategory = (category: string) => {};
+  const params = useLocalSearchParams<{ filters?: string }>();
+  const [selectedCategory, setSelectedCategory] = useState(
+    params.filters || "All"
+  );
+  const handleCategoryPress = (category: string) => {};
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt-3 mb-2'>
-        {categories.map((item, index) => (
-            <TouchableOpacity key={index} className='flex flex-col items-start mr-4 px-4 rounded-full'>
-                <Text>{item.title}</Text>
-            </TouchableOpacity>
-        ))}
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="mt-3 mb-2"
+    >
+      {categories.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() => handleCategoryPress(item.category)}
+          className={`flex flex-col items-start mr-4 px-4 rounded-full ${
+            selectedCategory === item.category
+              ? "bg-primary-300"
+              : "bg-primary-100 border border-primary-200"
+          }`}
+        >
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
+      ))}
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Filters
+export default Filters;
