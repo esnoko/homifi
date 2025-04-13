3dc5e66ea5bdc02676cc09bc32ffcffb83c541a8
esnoko <elvisnoko18@gmail.com>
2025-04-13 23:52:12 +0200

    Refactor Filters component to add category selection and update layout

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 const Filters = () => {
+    const params = useLocalSearchParams<{filters?: string}>();
+    const [selectedCategory, setSelectedCategory] = useState(params.filters || "All");
   return (
     <View>
       <Text>Filters</Text>
