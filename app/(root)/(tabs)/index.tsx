c24b733d67f6229702259f1886f074bfd511344e
esnoko <elvisnoko18@gmail.com>
2025-04-13 23:41:29 +0200

    Refactor Cards component to add "See All" button and update styling

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -26,11 +26,18 @@ export default function Index() {
     <View className="my-5 m-4">
       <View className="flex flex-row items-center justify-between">
         <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+        <TouchableOpacity>
         <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+        </TouchableOpacity>
       </View>
-      <FeaturedCards/>
-      <Cards/>
+      <View className="flex flex-row gap-5 mt-5">
+        <FeaturedCards/>
+        <FeaturedCards />
+        <FeaturedCards />
+      </View>
     </View>
+
+    
 
   </SafeAreaView>
   );
