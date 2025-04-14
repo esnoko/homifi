46aba9e08658ca2bd03aed6eb1280114d1a31ec8
esnoko <elvisnoko18@gmail.com>
2025-04-14 21:27:00 +0200

    Refactor Filters component to update category selection and layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,59 +3,51 @@ import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Link } from "expo-router";
 import React from "react";
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 
 export default function Index() {
   return (
   <SafeAreaView className="bg-white h-full" >
-    <View className="px-5">
-      <View className="flex flex-row items-center justify-between mt-5" >
-        <View className="flex flex-row items-center">
-          <Image source={images.avatar} className="size-12 rounded-full"/>
-          <View className="flex flex-col items-start ml-2 justify-center">
-            <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-            <Text className="text-base font-rubik-medium text-black-300">NokoDev</Text>
+
+    <FlatList
+      data={[1, 2, 3 , 4]}
+      renderItem={({item}) => <Cards/>}
+      ListHeaderComponent={
+        <><View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full" />
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                <Text className="text-base font-rubik-medium text-black-300">NokoDev</Text>
+              </View>
+            </View>
+            <Image source={icons.bell} className="size-6" />
           </View>
-        </View>
-        <Image source={icons.bell} className="size-6"/>
-      </View>
-    </View>
-    <Search/>
-    <View className="my-5 m-4">
-      <View className="flex flex-row items-center justify-between">
-        <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
-        <TouchableOpacity>
-        <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
-        </TouchableOpacity>
-      </View>
-      <View className="flex flex-row gap-5 mt-5">
-        <FeaturedCards/>
-        <FeaturedCards />
-        <FeaturedCards />
-      </View>
+        </View><Search /><View className="my-5 m-4">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity>
+            </View>
+            <View className="flex flex-row items-center justify-between mt-4">
+              <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity>
+            </View>
 
-      <View className="flex flex-row items-center justify-between mt-4">
-        <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
-        <TouchableOpacity>
-        <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
-        </TouchableOpacity>
-      </View>
-
-      <Filters />
-      
-      <View className="flex flex-row gap-5 mt-5">
-        <Cards/>
-        <Cards/>
-      </View>
-    </View>
-
+            <Filters />
+          </View></>
+      }
+    />
 
 
     
-
   </SafeAreaView>
   );
 }
